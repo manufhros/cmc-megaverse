010c9a5ff3121f0ba62808e0e10c9000b46e2e94
Manuel Hidalgo Ros <37451028+manufhros@users.noreply.github.com>
2024-06-04 21:31:54 +0200

    Delete phase1 directory

--- phase1/config.js
+++
@@ -1,4 +0,0 @@
-export const candidateId = 'dc640be1-d806-4a03-aebb-0ee7623faa6c';
-export const baseUrl = 'https://challenge.crossmint.io/api';
-export const N = 11; // Size of the map
-export const STARTING_POSITION = 2; // Position of the first X
